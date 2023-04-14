6b2ea29953e124060a6998615bbc9d943df9e51a
987cyh <987cyh@gmail.com>
2023-04-15 00:04:50 +0900

    IS NULL

---
+++ 02 SQL High Score KIT/04 IS NULL/02 이름이 없는 동물의 아이디.sql
@@ -0,0 +1,16 @@
+/*
+□ 참고 : https://school.programmers.co.kr/learn/challenges?tab=sql_practice_kit
+□ 목적 : IS NULL 복습(학습)
+*/
+
+-- MySQL
+SELECT ANIMAL_ID
+    FROM ANIMAL_INS
+    WHERE NAME IS NULL
+    ORDER BY ANIMAL_ID ASC;
+
+-- Oracle
+SELECT ANIMAL_ID
+    FROM ANIMAL_INS
+    WHERE NAME IS NULL
+    ORDER BY ANIMAL_ID ASC;
