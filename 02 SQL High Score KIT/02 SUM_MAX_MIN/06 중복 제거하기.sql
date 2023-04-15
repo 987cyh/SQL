f3c57974e812843e803a95993b9f2c000b21a90a
987cyh <987cyh@gmail.com>
2023-04-15 10:45:16 +0900

    DISTINCT

---
+++ 02 SQL High Score KIT/02 SUM_MAX_MIN/06 중복 제거하기.sql
@@ -0,0 +1,12 @@
+/*
+□ 참고 : https://school.programmers.co.kr/learn/challenges?tab=sql_practice_kit
+□ 목적 : DISTINCT 복습(학습)
+*/
+
+-- MySQL
+SELECT COUNT(DISTINCT(NAME)) AS "COUNT"
+FROM ANIMAL_INS;
+
+-- Oracle
+SELECT COUNT(DISTINCT(NAME)) AS "COUNT"
+FROM ANIMAL_INS;
