4e5e2dfbf5a3cc9782b56b3e8c3f4bcd57e88bc5
987cyh <987cyh@gmail.com>
2023-04-15 11:11:07 +0900

    SELECT

---
+++ 02 SQL High Score KIT/01 SELECT/04 흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -0,0 +1,18 @@
+/*
+□ 참고 : https://school.programmers.co.kr/learn/challenges?tab=sql_practice_kit
+□ 목적 : SELECT 복습(학습)
+*/
+
+-- MySQL
+SELECT DR_NAME,DR_ID,MCDP_CD,DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS "HIRE_YMD"
+FROM DOCTOR
+WHERE MCDP_CD='CS' OR MCDP_CD='GS'
+-- WHERE MCDP_CD='CS' OR MCDP_CD='GS'
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
+
+-- Oracle
+SELECT DR_NAME,DR_ID,MCDP_CD,TO_CHAR(HIRE_YMD, 'YYYY-MM-DD') AS "HIRE_YMD"
+FROM DOCTOR
+-- WHERE MCDP_CD='CS' OR MCDP_CD='GS'
+WHERE MCDP_CD in ('CS', 'GS')
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
