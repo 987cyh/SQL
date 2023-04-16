0a3eca4ea42780f7622cbcc3c29d590aebe6fd97
987cyh <987cyh@gmail.com>
2023-04-16 21:18:51 +0900

    SELECT

---
+++ 02 SQL High Score KIT/01 SELECT/06 인기있는 아이스크림.sql
@@ -0,0 +1,14 @@
+/*
+□ 참고 : https://school.programmers.co.kr/learn/challenges?tab=sql_practice_kit
+□ 목적 : SELECT 복습(학습)
+*/
+
+-- MySQL
+SELECT FLAVOR
+FROM FIRST_HALF
+ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID;
+
+-- Oracle
+SELECT FLAVOR
+FROM FIRST_HALF
+ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID;
