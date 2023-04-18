ebbbdb34210fb14932f71b5de6d70f04fc1cb150
987cyh <987cyh@gmail.com>
2023-04-18 22:07:21 +0900

    GROUP BY(REGEXP)

---
+++ 02 SQL High Score KIT/03 GROUP BY/03 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
@@ -0,0 +1,21 @@
+/*
+□ 참고 : https://school.programmers.co.kr/learn/challenges?tab=sql_practice_kit
+□ 목적 : GROUP BY 복습(학습)
+*/
+
+-- MySQL
+SELECT CAR_TYPE, COUNT(CAR_ID) AS "CARS"
+FROM CAR_RENTAL_COMPANY_CAR
+-- WHERE OPTIONS LIKE '%시트%'
+-- WHERE REGEXP_LIKE(OPTIONS, '통풍시트|열선시트|가죽시트')
+WHERE OPTIONS REGEXP ('통풍시트|열선시트|가죽시트')
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE ASC;
+
+-- Oracle
+SELECT CAR_TYPE, COUNT(CAR_ID) AS "CARS"
+FROM CAR_RENTAL_COMPANY_CAR
+-- WHERE OPTIONS LIKE '%시트%'
+WHERE REGEXP_LIKE(OPTIONS, '통풍시트|열선시트|가죽시트')
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE ASC;
