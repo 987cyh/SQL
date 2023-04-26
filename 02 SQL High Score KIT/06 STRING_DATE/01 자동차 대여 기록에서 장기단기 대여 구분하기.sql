e1db90ab94402a9985768b10bff7ce7978d70454
987cyh <987cyh@gmail.com>
2023-04-26 23:24:44 +0900

    CASE

---
+++ 02 SQL High Score KIT/06 STRING_DATE/01 자동차 대여 기록에서 장기단기 대여 구분하기.sql
@@ -0,0 +1,26 @@
+/*
+□ 참고 : https://school.programmers.co.kr/learn/challenges?tab=sql_practice_kit
+□ 목적 : CASE 복습(학습)
+*/
+
+-- MySQL
+SELECT
+        HISTORY_ID,
+        CAR_ID,
+        DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE,
+        DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE,
+        CASE WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 30 THEN '장기 대여'
+        ELSE '단기 대여' END AS RENT_TYPE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE DATE_FORMAT(START_DATE, '%Y-%m') = '2022-09'
+ORDER BY HISTORY_ID DESC;
+
+-- Oracle
+SELECT HISTORY_ID, CAR_ID,
+       TO_CHAR(START_DATE,'RRRR-MM-DD') AS "START_DATE",
+       TO_CHAR(END_DATE,'RRRR-MM-DD') AS "END_DATE",
+       CASE WHEN END_DATE - START_DATE +1 >= 30 THEN '장기 대여'
+       ELSE '단기 대여' END AS "RENT_TYPE"
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE TO_CHAR(START_DATE,'RRRR-MM') = '2022-09'
+ORDER BY 1 DESC;
